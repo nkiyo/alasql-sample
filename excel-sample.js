4c0b9b0a7b78122ad4292f5d328f4714558860d7
nkiyo <nkiyo@hoge.com>
2021-01-15 08:47:39 +0900

    update

---
+++ excel-sample.js
@@ -0,0 +1,10 @@
+const alasql = require('alasql')
+
+// TODO set sheetid, range
+alasql.promise('SELECT * FROM XLS("サンプル資料.xls") WHERE [担当者] = "Bさん" AND [状況] = "完了"')
+  .then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(`${err}`);
+  });
+
